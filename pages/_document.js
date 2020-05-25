20b645ed85120ea7a65fc6dd65a839b96f0fc6af
Jaeriah Tay <jaeriahpasseri@gmail.com>
2020-05-25 16:27:39 -0700

    add additional required twitter tags

--- pages/_document.js
+++ pages/_document.js
@@ -38,6 +38,9 @@ class MyDocument extends Document {
           <meta property="og:image" content="https://cdn.chec.io/email/assets/marketing/demo-preview.png" />
           <meta property="og:description" content="A custom, open source demo storefront built with Next.js and Commerce.js. Take a look!" />
           <meta property="og:url" content="https://commercejs-demo-store.netlify.app" />
+          <meta property="twitter:title" content="Commerce.js | Demo storefront" />
+          <meta property="twitter:image" content="https://cdn.chec.io/email/assets/marketing/demo-preview.png" />
+          <meta property="twitter:description" content="A custom, open source demo storefront built with Next.js and Commerce.js. Take a look!" />
           <meta name="twitter:card" content="summary_large_image"></meta>
         </Head>
         <body>
