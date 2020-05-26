d924d2972f9d9324f01e7fd6a7b401742026254a
Jaeriah Tay <48780927+jaepass@users.noreply.github.com>
2020-05-25 18:12:37 -0700

    add robbie's twitter:creator suggestion
    Co-authored-by: Robbie Averill <robbie@chec.io>

--- pages/_document.js
+++ pages/_document.js
@@ -39,6 +39,7 @@ class MyDocument extends Document {
           <meta property="og:description" content="A custom, open source demo storefront built with Next.js and Commerce.js. Take a look!" />
           <meta property="og:url" content="https://commercejs-demo-store.netlify.app" />
           <meta property="twitter:title" content="Commerce.js | Demo storefront" />
+          <meta name="twitter:creator" content="@commercejs">
           <meta property="twitter:image" content="https://cdn.chec.io/email/assets/marketing/demo-preview.png" />
           <meta property="twitter:description" content="A custom, open source demo storefront built with Next.js and Commerce.js. Take a look!" />
           <meta name="twitter:card" content="summary_large_image"></meta>
