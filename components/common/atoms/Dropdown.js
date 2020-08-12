15d8e8c15d11d25bfe0c866a118ef3f4e17f8cfb
Guy Marriott <guy.the.person@gmail.com>
2020-08-12 10:45:03 -0700

    Revert "Revert "fix checkout bug where pages/checkout.js redirects to homepage on successful checkout""

--- components/common/atoms/Dropdown.js
+++ components/common/atoms/Dropdown.js
@@ -23,12 +23,13 @@ class Dropdown extends Component {
         >
           <p className="font-color-medium">{this.props.value || this.props.placeholder }</p>
           <img
+            alt="Downward symbol indicating opening of a dropdown"
             src="/icon/arrow-bottom.svg"
             className="w-20"
           />
           <select
             name={this.props.name}
-            disabled={this.props.disabled}
+            disabled={this.props.disabled || undefined}
             value={this.props.value}
             className="position-absolute top-0 right-0 bottom-0 left-0 opacity-0 pointer w-100">
             <option value="" disabled>
