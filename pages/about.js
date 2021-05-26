2d0ccbdbe6cb1ca597564a8c2288ce544633f20d
Paito Anderson <pj.paito@gmail.com>
2021-05-26 13:37:38 -0400

    Update to Next 10.x
    Fixes #139

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Head from 'next/head';
 import Root from '../components/common/Root';
 import Footer from '../components/common/Footer';
 
 const About = () => (
   <Root>
+    <Head>
+      <title>About | commerce</title>
+    </Head>
     <div className="about-container">
       {/* Row */}
       <div className="row mt-5 pt-5 about-hero">
