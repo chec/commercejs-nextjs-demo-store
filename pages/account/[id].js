f9c60dd5d53cbe7e541016b252f5fccc315bd867
Paito Anderson <pj.paito@gmail.com>
2021-06-02 15:26:08 -0400

    Update pages/account/[id].js
    Co-authored-by: Jaeriah Tay <48780927+jaepass@users.noreply.github.com>

--- pages/account/[id].js
+++ pages/account/[id].js
@@ -136,7 +136,7 @@ export default function SingleOrderPage() {
    * Render loading state
    */
   if (customerLoading) {
-    return <TemplatePage page={ {message: 'Loading'} } />
+    return <TemplatePage page={  { message: 'Loading...' }  } />
   }
 
   /**
