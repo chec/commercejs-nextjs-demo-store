5ca7f53f07ece51278f2078d75a2c61a77a29483
Paito Anderson <pj.paito@gmail.com>
2021-07-06 13:03:51 -0400

    Fix displaying new products without redeploying

--- pages/product/[permalink].js
+++ pages/product/[permalink].js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import commerce from '../../lib/commerce';
 import { Collapse } from 'react-collapse';
-import { connect } from 'react-redux';
 import Head from 'next/head';
 import Root from '../../components/common/Root';
 import CarouselImages from '../../components/productAssets/CarouselImages';
@@ -18,113 +17,100 @@ const detailView = `<p>
   Slightly textured fabric with tonal geometric design and a bit of shine
 </p>`;
 
-class Product extends Component {
-  constructor(props) {
-    super(props);
+export default function Product({ product }) {
+  const [showShipping, setShowShipping] = React.useState(false);
+  const [showDetails, setShowDetails] = React.useState(false);
 
-    this.state = {
-      showShipping: false,
-      showDetails: false,
-    };
-
-    this.toggleShipping = this.toggleShipping.bind(this);
-    this.toggleDetails = this.toggleDetails.bind(this);
+  const toggleShipping = () => {
+    setShowShipping(!showShipping);
   }
 
-  toggleShipping() {
-    const { showShipping } = this.state;
-    this.setState({ showShipping: !showShipping });
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
   }
 
-  toggleDetails() {
-    const { showDetails } = this.state;
-    this.setState({ showDetails: !showDetails });
+  // For rendering /product/[permalink]
+  if (!product) {
+    return null;
   }
 
-  render() {
-    const { showShipping,showDetails } = this.state;
-    const { product } = this.props;
+  const images = reduceProductImages(product);
+  return (
+    <Root>
+      <Head>
+        <title>{ product.name } | commerce</title>
+      </Head>
 
-    const images = reduceProductImages(product);
+      <div className="py-5 my-5">
+      <div className="main-product-content">
+        {/* Sidebar */}
+        <div className="product-sidebar">
+          <CategoryList
+            className="product-left-aside__category-list"
+            current={ product.categories[0] && product.categories[0].id }
+          />
+          <CarouselImages images={images} />
+        </div>
 
-
-    return (
-      <Root>
-        <Head>
-          <title>{ product.name } | commerce</title>
-        </Head>
-
-        <div className="py-5 my-5">
-        <div className="main-product-content">
-          {/* Sidebar */}
-          <div className="product-sidebar">
-            <CategoryList
-              className="product-left-aside__category-list"
-              current={ product.categories[0] && product.categories[0].id }
-            />
-            <CarouselImages images={images} />
-          </div>
-
-          <div className="product-images">
-            <div className="flex-grow-1">
-              {Array.isArray(images) ? (images.map((image, i) => (
-                <img
-                  key={i}
-                  src={image}
-                  className="w-100 mb-3 carousel-main-images"
-                />
-              ))) : (
-                ''
-              )}
-            </div>
-          </div>
-
-          {/* Right Section - Product Details */}
-          <div className="product-detail">
-            <ProductDetail product={product} />
-
-            <div
-              onClick={this.toggleShipping}
-              className="d-flex cursor-pointer py-3 justify-content-between font-weight-medium"
-            >
-              Shipping and returns
-              <img src="/icon/plus.svg" />
-            </div>
-            <Collapse isOpened={showShipping}>
-              <div className="pb-4 font-color-medium">
-                Arrives in 5 to 7 days, returns accepted within 30
-                days. For more information, click here.
-              </div>
-            </Collapse>
-            <div className="h-1 border-bottom border-color-black" />
-            <div
-              onClick={this.toggleDetails}
-              className="d-flex cursor-pointer py-3 justify-content-between font-weight-medium"
-            >
-              Details
-              <img src="/icon/plus.svg" />
-            </div>
-            <Collapse isOpened={showDetails}>
-              <div
-                className="pb-4 font-color-medium"
-                dangerouslySetInnerHTML={{
-                  __html: detailView
-                }}
+        <div className="product-images">
+          <div className="flex-grow-1">
+            {Array.isArray(images) ? (images.map((image, i) => (
+              <img
+                key={i}
+                src={image}
+                className="w-100 mb-3 carousel-main-images"
               />
-            </Collapse>
-            <div className="h-1 borderbottom border-color-black" />
+            ))) : (
+              ''
+            )}
           </div>
         </div>
-      </div>
 
-      <ClientReview />
-      <SuggestedProducts />
-      <ExploreBanner />
-      <SocialMedia />
-      <Footer />
-    </Root>
-    );
-  }
+        {/* Right Section - Product Details */}
+        <div className="product-detail">
+          <ProductDetail product={product} />
+
+          <div
+            onClick={toggleShipping}
+            className="d-flex cursor-pointer py-3 justify-content-between font-weight-medium"
+          >
+            Shipping and returns
+            <img src="/icon/plus.svg" />
+          </div>
+          <Collapse isOpened={showShipping}>
+            <div className="pb-4 font-color-medium">
+              Arrives in 5 to 7 days, returns accepted within 30
+              days. For more information, click here.
+            </div>
+          </Collapse>
+          <div className="h-1 border-bottom border-color-black" />
+          <div
+            onClick={toggleDetails}
+            className="d-flex cursor-pointer py-3 justify-content-between font-weight-medium"
+          >
+            Details
+            <img src="/icon/plus.svg" />
+          </div>
+          <Collapse isOpened={showDetails}>
+            <div
+              className="pb-4 font-color-medium"
+              dangerouslySetInnerHTML={{
+                __html: detailView
+              }}
+            />
+          </Collapse>
+          <div className="h-1 borderbottom border-color-black" />
+        </div>
+      </div>
+    </div>
+
+    <ClientReview />
+    <SuggestedProducts />
+    <ExploreBanner />
+    <SocialMedia />
+    <Footer />
+  </Root>
+  );
 }
 
 /**
@@ -144,7 +130,7 @@ export async function getStaticPaths() {
   return {
     paths,
     // { fallback: false } means other routes should 404.
-    fallback: false,
+    fallback: true,
   }
 }
 
@@ -152,14 +138,13 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { permalink } }) {
   // params contains the product `permalink`.
   // If the route is like /product/shampoo-conditioner, then params.permalink is shampoo-conditioner
-  const product = await commerce.products.retrieve(permalink, { type: 'permalink '});
+  try {
+    const product = await commerce.products.retrieve(permalink, { type: 'permalink '});
 
-  // Pass product data to the page via props
-  return {
-    props: {
-      product,
-    },
-  };
+    // Pass product data to the page via props
+    return product ? { props: { product } } : { notFound: true };
+  } catch (error) {
+    console.error(error);
+    return { notFound: true };
+  }
 }
-
-export default connect(state => state)(Product);
