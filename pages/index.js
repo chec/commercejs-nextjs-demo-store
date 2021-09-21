b5525616366ed2c651ab5e306a8477dcca79d2c7
Kyle <36721153+kvisca@users.noreply.github.com>
2021-09-21 15:19:59 -0400

    Update index.js
    Adding meta information

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ const Home = () => (
   <Root transparentHeader={true}>
     <Head>
       <title>Home | commerce</title>
+      <meta name="google-site-verification" content="p8VRTWvFo5YWrtDwBWubFM45suUUUqEWVtVIU0qrdd4" />
     </Head>
 
     <HeroSection />
